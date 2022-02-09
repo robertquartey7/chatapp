42a0002ff229bf829a5bb334e338758b0cec5265
robertquartey7 <37972474+robertquartey7@users.noreply.github.com>
2022-02-08 22:31:14 -0500

    .

--- app.js
+++
@@ -1,8 +0,0 @@
-const http = require("http");
-const server = http.createServer((req, res) => {
-  console.log(req.url, req.method);
-});
-
-server.listen(3000, function (req, res) {
-  console.log("server is running on port 3000");
-});
